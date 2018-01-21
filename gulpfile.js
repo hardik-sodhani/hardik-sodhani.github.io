59ce368644ec645d51d4b2501522e0404023c906
hardik.sodhani <hardik.sodhani@accenture.com>
2018-01-21 15:09:09 -0600

    changing folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,27 @@ var sass        = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'docs/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("docs/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
-// Move the javascript files into our /docs/js folder
+// Move the javascript files into our /src/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
-        .pipe(gulp.dest("docs/js"))
+        .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        server: "./docs"  
+        server: "./"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'docs/scss/*.scss'], ['sass']);
-    gulp.watch("docs/*.html").on('change', browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['js','serve']);
