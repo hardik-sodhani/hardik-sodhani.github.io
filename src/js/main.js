7f8172289d8ae73ce5be72d88cdf108efc91ac63
hardik.sodhani <hardik.sodhani@accenture.com>
2018-03-07 00:21:37 -0600

    added navbar and completed contact section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-document.querySelector(".card-flip").classList.toggle("flip");
+// function to toggle class for card flip
+var bookCards = document.getElementsByClassName('card-flip');
+
+for (var i = 0; i < bookCards.length; i++) {
+    bookCards[i].addEventListener("click", function() {
+        this.classList.toggle("flip");
+    })
+}
